39e192d07f0baeafba8ab6173fccaf22fa216adc
Matthew Jourard <mjourard@gmail.com>
2021-07-17 00:33:20 -0400

    Issue #1 (#11)
    - added react-bootstrap-icons for icon support
    - changed the 'Choose Recipes' screen to use icons for inspecting a recipe and being able to remove it.

--- client/src/components/recipes-list.component.js
+++ client/src/components/recipes-list.component.js
@@ -1,10 +1,12 @@
 import { Component } from "react";
 import RecipeReadonly from "./recipe-readonly.component";
+import {Search, XCircle} from 'react-bootstrap-icons';
 
 export default class RecipesList extends Component {
     constructor(props) {
         super(props);
         this.setActiveRecipe = this.setActiveRecipe.bind(this);
+        this.removeRecipe = this.removeRecipe.bind(this);
         this.state = {
             currentRecipe: null,
             currentIndex: -1
@@ -18,9 +20,12 @@ export default class RecipesList extends Component {
         });
     }
 
+    removeRecipe(recipe, index) {
+        this.props.removeRecipe(recipe, index);
+    }
+
     render() {
         const { currentRecipe, currentIndex } = this.state;
-
         return (
             <div className="list row">
                 <div className="col-md-6">
@@ -32,10 +37,16 @@ export default class RecipesList extends Component {
                                     "list-group-item " +
                                     (index === currentIndex ? "active" : "")
                                 }
-                                onClick={() => this.setActiveRecipe(recipe, index)}
+
                                 key={index}
                             >
-                                {recipe.name}
+                                <div className="d-flex justify-content-start align-items-stretch">
+                                    {recipe.name}
+                                    <div className="d-flex justify-content-end align-items-center flex-fill border-dark border-1">
+                                        <Search onClick={() => this.setActiveRecipe(recipe, index)} className="ms-2 me-2" role="button"/>
+                                        <XCircle onClick={() => this.removeRecipe(recipe, index)} className="text-danger ms-2 me-2" role="button"/>
+                                    </div>
+                                </div>
                             </li>
                         ))}
                     </ul>
