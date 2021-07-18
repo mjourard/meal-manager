c97323d8cb2cb75602e7d3c77748c00d2ac034a5
matt.jourard <mjourard@gmail.com>
2021-07-18 18:55:29 -0400

    Resolves #15
    - read about the the build steps for react and realized I could update the baseURL with just a .env file, so that was added

--- client/src/http-common.js
+++ client/src/http-common.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
     //TODO: update baseURL to be set during the build phase
-    baseURL: "http://localhost:8080/api",
+    baseURL: process.env.REACT_APP_MEALMANAGER_BASE_URL + "/api",
     headers: {
         "Content-type": "application/json"
     }
